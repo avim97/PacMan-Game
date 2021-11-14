c815351c8332de71043907215556e287fbbdc3a9
Avi Maya <avima@mta.ac.il>
2021-11-14 19:26:20 +0200

    psik

--- PacMan-Game/Pacman.cpp
+++ PacMan-Game/Pacman.cpp
@@ -4,7 +4,7 @@
 void Pacman::Move(char nextDir)
 {
 
-	// here we need to check if theres a ghost in the next step, or not and then check if theres food or space
+	// here we need to check if there's a ghost in the next step, or not and then check if there's food or space
 	switch (nextDir)
 	{
 		// 
