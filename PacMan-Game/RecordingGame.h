7cc06341316756127327ae0d15eb57065ffa914f
Avi Maya <avimaya029@gmail.com>
2022-01-04 18:26:43 +0200

    changes

--- PacMan-Game/RecordingGame.h
+++ PacMan-Game/RecordingGame.h
@@ -2,6 +2,7 @@
 #include "InteractiveGame.h"
 #include "GameFiles.h"
 
+
 class RecordingGame : public Game
 {
 private:
@@ -14,12 +15,15 @@ public:
 		
 	{
 		srand((unsigned int)time(nullptr));
+		InitializeGhostsVector(m_Board.getGhostInitPos());
 	};
 
 	RecordingGame(string& boardPath, GameMode mode, int lives, int score, GameFiles& GameFiles) :
 		Game(boardPath, mode, lives, score),
 		m_GameFiles(GameFiles)
 	{
+		m_Pacman.UpdateLife(lives);
+		InitializeGhostsVector(m_Board.getGhostInitPos());
 		srand((unsigned int)time(nullptr));
 	};
 	void RecordSteps(bool &ghostsMoved, bool &fruitMoved);
